042c05421fb452e3ca91a8d27a879d4d5c0c6ade
dalimil23 <103389387+dalimil23@users.noreply.github.com>
2022-04-11 01:06:14 +0200

    Revert "2.2.6"
    This reverts commit 7b15992885598a239d06cd0308daa852bf85b87a.

--- extensions/SerpentineLayout.js
+++ extensions/SerpentineLayout.js
@@ -21,7 +21,6 @@
 * positioning nodes in horizontal rows back and forth, alternating between left-to-right
 * and right-to-left within the {@link #wrap} limit.
 * {@link #spacing} controls the distance between nodes.
-* {@link #leftSpot} and {@link #rightSpot} determine the Spots to use for the {@link Link#fromSpot} and {@link Link#toSpot}.
 * <p/>
 * When this layout is the Diagram.layout, it is automatically invalidated when the viewport changes size.
 */
@@ -30,9 +29,6 @@ function SerpentineLayout() {
   this.isViewportSized = true;
   this._spacing = new go.Size(30, 30);
   this._wrap = NaN;
-  this._root = null;
-  this._leftSpot = go.Spot.Left;
-  this._rightSpot = go.Spot.Right;
 }
 go.Diagram.inherit(SerpentineLayout, go.Layout);
 
@@ -46,9 +42,6 @@ SerpentineLayout.prototype.cloneProtected = function(copy) {
   go.Layout.prototype.cloneProtected.call(this, copy);
   copy._spacing = this._spacing;
   copy._wrap = this._wrap;
-  // don't copy _root
-  copy._leftSpot = this._leftSpot;
-  copy._rightSpot = this._rightSpot;
 };
 
 /**
@@ -62,24 +55,23 @@ SerpentineLayout.prototype.doLayout = function(coll) {
   var diagram = this.diagram;
   coll = this.collectParts(coll);
 
-  var root = this.root;
-  if (root === null) {
-    // find a root node -- one without any incoming links
-    var it = coll.iterator;
-    while (it.next()) {
-      var n = it.value;
-      if (!(n instanceof go.Node)) continue;
-      if (root === null) root = n;
-      if (n.findLinksInto().count === 0) {
-        root = n;
-        break;
-      }
+  var root = null;
+  // find a root node -- one without any incoming links
+  var it = coll.iterator;
+  while (it.next()) {
+    var n = it.value;
+    if (!(n instanceof go.Node)) continue;
+    if (root === null) root = n;
+    if (n.findLinksInto().count === 0) {
+      root = n;
+      break;
     }
   }
   // couldn't find a root node
   if (root === null) return;
 
   var spacing = this.spacing;
+
   // calculate the width at which we should start a new row
   var wrap = this.wrap;
   if (diagram !== null && isNaN(wrap)) {
@@ -103,17 +95,10 @@ SerpentineLayout.prototype.doLayout = function(coll) {
   var increasing = true;
   var node = root;
   while (node !== null) {
-    var orignode = node;
-    if (node.containingGroup !== null) node = node.containingGroup;
     var b = this.getLayoutBounds(node);
     // get the next node, if any
-    var nextlink = null;
-    for (var it = orignode.findLinksOutOf().iterator; it.next();) {
-      if (coll.has(it.value)) { nextlink = it.value; break; }
-    }
+    var nextlink = node.findLinksOutOf().first();
     var nextnode = (nextlink !== null ? nextlink.toNode : null);
-    var orignextnode = nextnode;
-    if (nextnode !== null && nextnode.containingGroup !== null) nextnode = nextnode.containingGroup;
     var nb = (nextnode !== null ? this.getLayoutBounds(nextnode) : new go.Rect());
     if (increasing) {
       node.move(new go.Point(x, y));
@@ -125,14 +110,14 @@ SerpentineLayout.prototype.doLayout = function(coll) {
         rowh = 0;
         increasing = false;
         if (nextlink !== null) {
-          nextlink.fromSpot = this._rightSpot;
-          nextlink.toSpot = this._rightSpot;
+          nextlink.fromSpot = go.Spot.Right;
+          nextlink.toSpot = go.Spot.Right;
         }
       } else {
         x += spacing.width;
         if (nextlink !== null) {
-          nextlink.fromSpot = this._rightSpot;
-          nextlink.toSpot = this._leftSpot;
+          nextlink.fromSpot = go.Spot.Right;
+          nextlink.toSpot = go.Spot.Left;
         }
       }
     } else {
@@ -145,18 +130,18 @@ SerpentineLayout.prototype.doLayout = function(coll) {
         rowh = 0;
         increasing = true;
         if (nextlink !== null) {
-          nextlink.fromSpot = this._leftSpot;
-          nextlink.toSpot = this._leftSpot;
+          nextlink.fromSpot = go.Spot.Left;
+          nextlink.toSpot = go.Spot.Left;
         }
       } else {
         x -= spacing.width;
         if (nextlink !== null) {
-          nextlink.fromSpot = this._leftSpot;
-          nextlink.toSpot = this._rightSpot;
+          nextlink.fromSpot = go.Spot.Left;
+          nextlink.toSpot = go.Spot.Right;
         }
       }
     }
-    node = orignextnode;
+    node = nextnode;
   }
 
   if (diagram !== null) diagram.commitTransaction("Serpentine Layout");
@@ -169,6 +154,7 @@ SerpentineLayout.prototype.doLayout = function(coll) {
 * and whose height specifies the minimum vertical space between nodes.
 * The default value is 30x30.
 * @name SerpentineLayout#spacing
+
 * @return {Size}
 */
 Object.defineProperty(SerpentineLayout.prototype, "spacing", {
@@ -187,6 +173,7 @@ Object.defineProperty(SerpentineLayout.prototype, "spacing", {
 * The default value is NaN, which for {@link Diagram#layout}s means that it uses
 * the {@link Diagram#viewportBounds}.
 * @name SerpentineLayout#wrap
+
 * @return {number}
 */
 Object.defineProperty(SerpentineLayout.prototype, "wrap", {
@@ -194,54 +181,6 @@ Object.defineProperty(SerpentineLayout.prototype, "wrap", {
   set: function(val) {
     if (this._wrap !== val) {
       this._wrap = val;
-      this.invalidateLayout();
-    }
-  }
-});
-
-/**
-* Gets or sets the starting node of the sequence.
-* The default value is null, which causes the layout to look for a node without any incoming links.
-* @name SerpentineLayout#root
-* @return {Node}
-*/
-Object.defineProperty(SerpentineLayout.prototype, "root", {
-  get: function() { return this._root; },
-  set: function(val) {
-    if (this._root !== val) {
-      this._root = val;
-      this.invalidateLayout();
-    }
-  }
-});
-
-/**
-* Gets or sets the Spot to use on the left side of a Node.
-* The default value is {@link Spot.Left}.
-* @name SerpentineLayout#leftSpot
-* @return {number}
-*/
-Object.defineProperty(SerpentineLayout.prototype, "leftSpot", {
-  get: function() { return this._leftSpot; },
-  set: function(val) {
-    if (this._leftSpot !== val) {
-      this._leftSpot = val;
-      this.invalidateLayout();
-    }
-  }
-});
-
-/**
-* Gets or sets the Spot to use on the right side of a Node.
-* The default value is {@link Spot.Right}.
-* @name SerpentineLayout#rightSpot
-* @return {number}
-*/
-Object.defineProperty(SerpentineLayout.prototype, "rightSpot", {
-  get: function() { return this._rightSpot; },
-  set: function(val) {
-    if (this._rightSpot !== val) {
-      this._rightSpot = val;
       this.invalidateLayout();
     }
   }
